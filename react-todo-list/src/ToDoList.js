ee5ee28accc4a1d4bbe94afb93af0755c3b4f2ca
Kavin <pinghangfan1022@gmail.com>
2021-07-13 01:38:51 +1200

    added interface, set up sample todo list, added function to add item to todo list

--- react-todo-list/src/ToDoList.js
+++ react-todo-list/src/ToDoList.js
@@ -1,7 +1,29 @@
-const ToDoList = () => {
-    return (
-        <h1>Hiii</h1>
+import { useState } from "react";
 
+const ToDoList = () => {
+
+    const [toDo, setTodo] = useState([{body: "Buy milk", id: 1}, {body: "Wash cloths", id: 2}]);
+    const [currentToDo, setCurrentToDo] = useState("");
+
+    const handleClick = (curr) => {
+        if (curr != ""){
+            let tempTodo = toDo.slice();
+            let newItem = {body: curr, id: toDo.length + 1}
+            tempTodo.push(newItem);
+            setTodo(tempTodo);
+            setCurrentToDo("");
+        }
+    }
+
+    return (
+        <div id="to-do-wrapper">
+            <div id="to-do-array">
+                {toDo.map((x) => <li key={x.id}>{x.body}</li>)}
+            </div>
+
+            <textarea id="todo-textarea" type="text" value={currentToDo} onChange={(e => setCurrentToDo(e.target.value))}></textarea>
+            <button onClick={() => handleClick(currentToDo)} id="add-button">Add</button>
+        </div>
     ); 
 }
 
